15ebef263fc8db270b99d2c266d11b7e045a5a4c
Gopika V <gopikavijayakumar2506@gmail.com>
2026-06-21 14:34:48 +0530

    Create Comment.js

---
+++ Comment.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  postId: String,
+  comment: String
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
